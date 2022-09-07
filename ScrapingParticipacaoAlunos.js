911fad02270a20ade4f5380c0397c697d9d10700
Delon Henrique <delon.henrique@gmail.com>
2022-09-07 20:47:04 -0300

    Em desenvolvimento

--- ScrapingParticipacaoAlunos.js
+++ ScrapingParticipacaoAlunos.js
@@ -3,62 +3,50 @@ let dataStudents
 let turma
 let prova
 let disciplina
+
+function getCombo(id, jsonAtrib) {
+    let combo = document.getElementById(id);
+    let dataComboSelected = combo.options[combo.selectedIndex].text;
+    console.log(JSON.stringify(jsonAtrib) + ": " + JSON.stringify(dataComboSelected));
+    return dataComboSelected
+}
+
+prova = getCombo('selectnn74f9d641c4DADOS.VL_FILTRO_AVALIACAO', 'Avaliacao')
+
+console.log(" ");
+
+disciplina = getCombo('select3lw7fc885326DADOS.VL_FILTRO_DISCIPLINA', 'Disciplina')
+
+console.log(" ");
+
+turma = getCombo('selecty0fuc3bed07cDADOS.VL_FILTRO_ETAPA', 'AnoSerie')
+
+console.log(" ");
+
 Array.from($$("body table tr *")).forEach((el) => {
     el = String(el.innerHTML)
     dataStudents += el;
 
     if (el.indexOf('Atualizado') === 0) {
-        updateTime = "Data: " + el
-    } else if(el.length != 0 && el != undefined) {
+        updateTime = el
+    } else if (el.length != 0 && el != undefined) {
         console.log(el);
     }
 
 });
+console.log(updateTime);
 
-console.log(" ");
-
-let selectTurma = document.getElementById('selecty0fuc3bed07cDADOS.VL_FILTRO_ETAPA');
-turma = selectTurma.options[selectTurma.selectedIndex].text;
-console.log(turma);
-dataStudents += turma
-
-console.log(" ");
-
-let selectProva = document.getElementById('selectnn74f9d641c4DADOS.VL_FILTRO_AVALIACAO');
-prova = selectProva.options[selectProva.selectedIndex].text;
-console.log(prova);
-dataStudents += prova
-
-
-console.log(" ");
-
-let selectDisciplina = document.getElementById('select3lw7fc885326DADOS.VL_FILTRO_DISCIPLINA');
-disciplina = selectDisciplina.options[selectDisciplina.selectedIndex].text;
-console.log(disciplina);
-dataStudents += disciplina
-
-
-// Array.from($$("body select *")).forEach((el) => {
-    
-
-//     if (el.length != 0 && el != undefined) {
-//         turma = el.value
-//         if (true)
-//             console.log(el);
-//     }
-
-// });
+console.log(JSON.stringify({prova: prova, disciplina: disciplina, turma: turma}))
 
 //Bloco para exportar o arquivo
-let blob = new Blob([dataStudents]);
-let url = URL.createObjectURL(blob);
-let file = document.createElement(`a`);
-file.download = `file.json`;
-file.href = url;
-document.body.appendChild(file);
-file.click();
-file.remove();
-URL.revokeObjectURL(url);
+// let blob = new Blob([dataStudents]);
+// let url = URL.createObjectURL(blob);
+// let file = document.createElement(`a`);
+// file.download = `file.json`;
+// file.href = url;
+// document.body.appendChild(file);
+// file.click();
+// file.remove();
+// URL.revokeObjectURL(url);
 
 
-console.log(updateTime);
